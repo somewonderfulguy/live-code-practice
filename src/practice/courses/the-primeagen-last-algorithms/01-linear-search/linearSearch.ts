6d3e74f5bb9da29bebd8767ebe48dae4cef2b374
somewonderfulguy <somewonderfulguy@gmail.com>
2026-09-04 13:21:52 +0300

    clear linearSearch

--- src/practice/courses/the-primeagen-last-algorithms/01-linear-search/linearSearch.ts
+++ src/practice/courses/the-primeagen-last-algorithms/01-linear-search/linearSearch.ts
@@ -1,7 +1,1 @@
-export function linearSearch<T>(haystack: T[], needle: T): boolean {
-  for (let i = 0; i < haystack.length; i++) {
-    if (haystack[i] === needle) return true
-  }
-
-  return false
-}
+export function linearSearch<T>(haystack: T[], needle: T): boolean {}
